31b44c48f77a157fd47f33f324820d2e90b8aa22
AranBeitia <arantza.beitia@gmail.com>
2021-10-05 17:54:50 +0200

    location

--- src/pages/Location/Location.js
+++ src/pages/Location/Location.js
@@ -3,13 +3,17 @@ import React from "react";
 import * as routes from "../../constants/routes";
 
 import Layout from "../../components/Layout";
+import LocationCard from "../../components/LocationCard";
 import createRequest from "../../utils/request";
 import axios from "axios";
+import CharacterCard from "../../components/CharacterCard";
 class Location extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: [],
+      location: [],
+      characters: [],
+      hasLoaded: false,
     };
   }
   async componentDidMount() {
@@ -22,12 +26,23 @@ class Location extends React.Component {
     axios
       .get(`https://rickandmortyapi.com/api${routes.LOCATION}/${locationId}`)
       .then((result) => {
-        this.setState({ locations: result.data });
+        const location = result.data;
+        this.setState({ location: location });
+        axios
+          .all(location.residents.map((i) => axios.get(i)))
+          .then((request) => {
+            const result = request.map((i) => i.data);
+            this.setState({
+              characters: result,
+              hasLoaded: true,
+            });
+          });
       });
   }
   render() {
-    const { locations } = this.state;
+    const { location, characters, hasLoaded } = this.state;
     const { history } = this.props;
+    console.log(characters);
     return (
       <Layout>
         <section className="row">
@@ -39,7 +54,19 @@ class Location extends React.Component {
           <div className="col col-12">
             <h5>Location</h5>
           </div>
-          <p>location: {locations.name}</p>
+          <LocationCard
+            name={location.name}
+            type={location.type}
+            dimension={location.dimension}
+          />
+          <div className="col col-12">
+            <h5>Residents</h5>
+          </div>
+          {hasLoaded &&
+            characters.map((resident) => {
+              // console.log(resident.name);
+              <p>{resident.name}</p>;
+            })}
         </section>
       </Layout>
     );
